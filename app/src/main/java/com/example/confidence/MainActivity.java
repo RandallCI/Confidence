365c74b35030f34c98f5d6f091cc2339613e21a1
Randall Isaacs <sorandall777@gmail.com>
2021-07-03 02:39:58 +0200

    Thank You LORD JESUS my GOD. I have set up my layout for the start page. Thank You JESUS my Saviour.

--- app/src/main/java/com/example/confidence/MainActivity.java
+++ app/src/main/java/com/example/confidence/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.confidence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView dayTitle;
+    ImageButton messageForToday;
+    Button newMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
