98e8b927728abf1dbcf4356f578e39dd1f4fffe8
Randall Isaacs <sorandall777@gmail.com>
2021-07-30 14:28:32 +0200

    Thank You so much LORD JESUS my Saviour and KING. I have added a test case to check if an internet connection is available before trying to connect to the internet and retrieving the message from the database. My one concern is that my project fails every second time when i run it than I need to clean the build folder but I will get it. Thank You LORD JESUS very, very, very much.

--- app/src/main/java/com/example/confidence/MainActivity.java
+++ app/src/main/java/com/example/confidence/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.confidence;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +20,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.lang.ref.WeakReference;
+
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -62,17 +65,15 @@ public class MainActivity extends AppCompatActivity {
 
 //               connectToDatabase();
         });
-        //Grab the new message from the database.
-        AsyncActivity asyncActivity = new AsyncActivity(MainActivity.this);
-        asyncActivity.execute();
+        //Get the data from the network if the user is connected to the internet.
+        ifNetworkIsAvailableGetMessage(MainActivity.this);
 
         theCurrentDay();
 
     }
 
     public void newMessage(View view) {
-        AsyncActivity asyncActivity = new AsyncActivity(MainActivity.this);
-        asyncActivity.execute();
+        ifNetworkIsAvailableGetMessage(MainActivity.this);
     }
 
 
@@ -83,6 +84,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Private methods
+
+    //Check for a valid internet connection before trying to fetch data from the database.
+    private void ifNetworkIsAvailableGetMessage(Context context) {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+
+        if (connectivityManager != null) {
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            if (activeNetworkInfo != null) {
+                if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                    //Grab the new message from the database.
+                    AsyncActivity asyncActivity = new AsyncActivity(MainActivity.this);
+                    asyncActivity.execute();
+
+                } else if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+                    //Grab the new message from the database.
+                    AsyncActivity asyncActivity = new AsyncActivity(MainActivity.this);
+                    asyncActivity.execute();
+                }
+            } else {
+               Toast toastB = Toast.makeText(this, "Hello, please connect to an internet connection to retrieve messages", Toast.LENGTH_LONG);
+
+               toastB.show();
+            }
+        }
+
+    }
+
+    //Fetch the data from the database.
     private void fetchMotivation() {
         documentReference.get().addOnSuccessListener(documentSnapshot -> {
             if (documentSnapshot.exists()) {
